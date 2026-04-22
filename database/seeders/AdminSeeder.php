40b9878305b2f1b5d68c1f7b763234c34b2847c9
20517005 <codingthunder1997@gmail.com>
2026-04-22 23:03:59 +0530

    Remove unused import for WithoutModelEvents in AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,9 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class AdminSeeder extends Seeder
 {
